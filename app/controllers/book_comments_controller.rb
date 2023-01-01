433919c9f6d63c4e3e2361cf815aa5e377934fba
EC2 Default User <ec2-user@ip-172-31-36-110.ap-northeast-1.compute.internal>
2023-01-01 09:26:27 +0000

    [add]Bookers2_debug_コメント機能追加

---
+++ app/controllers/book_comments_controller.rb
@@ -0,0 +1,25 @@
+class BookCommentsController < ApplicationController
+
+  def create
+    book = Book.find(params[:book_id])
+    comment = current_user.book_comments.new(book_params)
+    comment.book_id = book.id
+    if comment.save
+    redirect_to book_path(book.id)
+    else
+    redirect_to book_path(book.id)
+    end
+  end
+
+  def destroy
+    BookComment.find(params[:id]).destroy
+    redirect_to book_path(params[:book_id])
+  end
+
+  private
+
+  def book_params
+    params.require(:book_comment).permit(:comment)
+  end
+
+end
